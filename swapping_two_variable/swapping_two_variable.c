7ce3abb3a5797ddc830ece81fa58ca4086058fc4
Şevval İLHAN <77413445+ilhansevval@users.noreply.github.com>
2022-12-01 12:53:51 +0300

    swapping_two_variable

--- swapping_two_variable/swapping_two_variable.c
+++ swapping_two_variable/swapping_two_variable.c
@@ -18,7 +18,7 @@ int main() {
 	printf("After Swapping \n First variable:  %d \n Second variable = %d \n", var1, var2);
 
 	
-	//Swapping Two Numbers Using Without Using Third Variable
+	//Swapping Two Numbers Without Using Third Variable: Method 1 Using Arithmetic Operator
 	int var3, var4;
 	
 	printf("\n Enter two integer: ");
@@ -31,6 +31,34 @@ int main() {
 	var3 = var3 - var4;
 	
 	printf("After Swapping \n First variable: %d \n Second variable: %d \n", var3, var4);
+	
+	//Swapping Two Numbers Without Using Third Variable: Method 2 Using Arithmetic Operator
+	int var5, var6;
+	
+	printf("\n Enter two integer: ");
+	scanf("%d %d", &var5, &var6);
+	
+	printf("Before Swapping \n First variable: %d \n Second variable: %d \n", var5, var6);
+	
+	var5 = var5 * var6;
+	var6 = var5 / var6;
+	var5 = var5 / var6;
+	
+	printf("After Swapping \n First variable: %d \n Second variable: %d \n", var5, var6);
+
+	//Swapping Two Numbers Without Using Third Variable: Method 3 Using Bitwise XOR
+	int var7, var8;
+	
+	printf("\n Enter two integer: ");
+	scanf("%d %d", &var7, &var8);
+	
+	printf("Before Swapping \n First variable: %d \n Second variable: %d \n", var7, var8);
+	
+	var7 = var7 ^ var8;
+	var8 = var7 ^ var8;
+	var7 = var7 ^ var8;
+	
+	printf("After Swapping \n First variable: %d \n Second variable: %d \n", var7, var8);
 
 	return 0;
 }
