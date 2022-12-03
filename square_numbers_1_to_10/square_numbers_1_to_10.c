7fe682874c4337cbf4564ec82f4255a39ef2d686
Şevval İLHAN <77413445+ilhansevval@users.noreply.github.com>
2022-12-04 00:03:35 +0300

    square_numbers_1_to_10

---
+++ square_numbers_1_to_10/square_numbers_1_to_10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Print the Square of All Numbers from 1 to 10.
+
+int main() {
+	
+	int i;
+	
+	for (i=1; i<=10; i++)
+	{
+		printf("%d Square is %d\n",i,i*i);
+	}
+	
+	return 0;
+}
